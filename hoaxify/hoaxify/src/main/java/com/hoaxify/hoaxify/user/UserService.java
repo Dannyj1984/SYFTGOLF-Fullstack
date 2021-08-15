ad2ce56884db2a1da2e0b1758bc27e9ed282f7e0
Danny J <dj3636@ou.ac.uk>
2021-08-15 07:31:03 +0100

    get Users
    Chapter 59

--- hoaxify/hoaxify/src/main/java/com/hoaxify/hoaxify/user/UserService.java
+++ hoaxify/hoaxify/src/main/java/com/hoaxify/hoaxify/user/UserService.java
@@ -1,5 +1,8 @@
 package com.hoaxify.hoaxify.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +22,13 @@ public class UserService {
     public User save(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public Page<?> getUsers() {
+        //each page can have 10 items
+        Pageable pageable = PageRequest.of(0, 10);
+        return userRepository.findAll(pageable);
+
     }
 
 }
